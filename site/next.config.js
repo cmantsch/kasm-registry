0786b1f4fd7f590d44c0669d3669dbdac08eae63
Christoph Mantsch <cmantsch@users.noreply.github.com>
2024-05-15 19:38:25 +0000

    Initial configuration

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'CTM Workspaces Registry',
+    description: 'Custom images maintained by me.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://cmantsch.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/cmantsch/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
